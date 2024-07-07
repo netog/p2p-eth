4964a103104debff659997660d6ae8921f344e70
Ernesto Guevara <netog74@gmail.com>
2024-07-07 12:06:51 -0500

    Initial project structure

---
+++ src/p2p_eth.rs
@@ -0,0 +1,14 @@
+use bytes::{Bytes, BytesMut};
+pub struct Handshake {
+
+}
+
+impl Handshake{
+    pub fn new() -> Self {
+
+    }
+
+    pub fn auth(&mut self) -> BytesMut {
+    }
+
+}
---
+++ src/p2p_eth/error.rs
@@ -0,0 +1,6 @@
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+#[derive(Debug, Error)]
+pub enum Error {
+
+}
